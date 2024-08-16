ea3ff098d1c0552738606de9a9daf8251f000b5b
Simona <simonasablinaite@gmail.com>
2024-08-16 09:23:34 +0300

    listProducts component with style and respons

--- admin/src/Components/ListProducts/ListProducts.jsx
+++ admin/src/Components/ListProducts/ListProducts.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './listProducts.css'
+import remove_icon from '../../assets/remove-icon.png'
 
 const ListProducts = () => {
+
+   const [allproducts, setAllproducts] = useState([]);
+
+   const fetchInfo = async () => {
+      await fetch('http://localhost:7000/allproducts')
+         .then((res) => res.json())
+         .then((data) => { setAllproducts(data) });
+   }
+
+   useEffect(() => {
+      fetchInfo();
+   }, [])
+
    return (
       <div className='list-products'>
+         <h1>Visų produktų sąrašas</h1>
 
+         <div className="list-products-format-main">
+            <p>Produktas</p>
+            <p>Pavadinimas</p>
+            <p>Sena kaina</p>
+            <p>Nauja kaina</p>
+            <p>Kategorija</p>
+            <p>Pašalinti</p>
+         </div>
+
+         <div className="list-products-all-products">
+            <hr />
+            {allproducts.map((product, i) => {
+               return <>
+                  <div key={i} className="list-products-format-main list-products-format">
+                     <img className='list-products-product-icon' src={product.image} alt="product image" />
+
+                     <p>{product.name}</p>
+                     <p>{product.old_price} €</p>
+                     <p>{product.new_price} €</p>
+                     <p>{product.category}</p>
+
+                     <img className='list-products-remove-icon' src={remove_icon} alt="delete product ikon" />
+                  </div>
+                  <hr />
+               </>
+            })}
+         </div>
       </div>
    )
 }
